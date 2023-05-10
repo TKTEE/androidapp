81f6c13ef14f20a7f9d1e811c3dc03b7d19f7554
eMobilis <consolatamburu99@gmail.com>
2023-05-10 16:04:36 +0300

    app project

---
+++ app/src/main/java/com/example/showbobo/Relativelayoutactivity.kt
@@ -0,0 +1,11 @@
+package com.example.showbobo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Relativelayoutactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relativelayoutactivity)
+    }
+}
---
+++ app/src/main/java/com/example/showbobo/pagelinearactivity.kt
@@ -0,0 +1,11 @@
+package com.example.showbobo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pagelinearactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pagelinearactivity)
+    }
+}
